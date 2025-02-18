e86d6954e499eeaf3db1c999d84dc0d53dc7d644
Brent Amersbach <triumshockwave@gmail.com>
2025-02-18 16:30:39 -0500

    Initial commit Main functionality of creating the QR code done

--- QRGen/main.swift
+++ QRGen/main.swift
@@ -5,7 +5,28 @@
 //  Created by Brent Amersbach on 2/18/25.
 //
 
-import Foundation
+import Cocoa
 
-print("Hello, World!")
+//TODO: Get input string from stdin
 
+func generateQRCode(from string: String) -> NSImage? {
+    let data = string.data(using: String.Encoding.ascii)
+
+    if let filter = CIFilter(name: "CIQRCodeGenerator") {
+        filter.setValue(data, forKey: "inputMessage")
+        let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+        if let output = filter.outputImage?.transformed(by: transform) {
+            let rep = NSCIImageRep(ciImage: output)
+            let outputImage = NSImage(size: rep.size)
+            outputImage.addRepresentation(rep)
+            return outputImage
+        }
+    }
+
+    return nil
+}
+
+generateQRCode(from: "Hello World!")
+
+//TODO: Save image out to file
