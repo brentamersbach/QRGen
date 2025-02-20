b32a15d46bbe22948ad589a4a47e9018089ec73a
Brent Amersbach <triumshockwave@gmail.com>
2025-02-19 22:40:33 -0500

    Reorganized main.swift to use proper bootstrapping and slim down imports

--- QRGen/main.swift
+++ QRGen/main.swift
@@ -5,37 +5,44 @@
 //  Created by Brent Amersbach on 2/18/25.
 //
 
-import Cocoa
-import ArgumentParser
+import Foundation
 
 //TODO: Option to specify output file
-//TODO: Get input string from stdin
 
-print(String(CommandLine.arguments.count))
-var input: String = ""
-if CommandLine.arguments.count > 1 {
-    input = CommandLine.arguments.last ?? ""
+struct App {
+    static func main() {
+        print(String(CommandLine.arguments.count))
+        var input: String = ""
+        if CommandLine.arguments.count > 1 {
+            input = CommandLine.arguments.last ?? ""
+        }
+
+        if input.lengthOfBytes(using: .utf8) <= 0 {
+            print("Input text to encode:")
+            input = readLine(strippingNewline: true) ?? ""
+        }
+
+        if input.isEmpty {
+            exit(1)
+        }
+
+        #if DEBUG
+        var path = FileManager.default.homeDirectoryForCurrentUser.path
+        path.append("/Desktop/\(input).png")
+        #else
+        var path = FileManager.default.currentDirectoryPath
+        path.append("/\(input).png")
+        #endif
+
+        let generator = qrGenerator()
+        let qrCode = generator.generateQRCode(from: input)
+        do {
+            try generator.saveImage(qrCode!, to: path)
+        } catch {
+            print(error.localizedDescription)
+            exit(2)
+        }
+        exit(0)
+    }
 }
 
-if input.lengthOfBytes(using: .utf8) <= 0 {
-    print("Input text to encode:")
-    input = readLine(strippingNewline: true) ?? ""
-}
-
-if input.isEmpty {
-    exit(1)
-}
-
-#if DEBUG
-var path = FileManager.default.homeDirectoryForCurrentUser.path
-path.append("/Desktop/\(input).png")
-#else
-var path = FileManager.default.currentDirectoryPath
-path.append("/\(input).png")
-#endif
-
-let generator = qrGenerator()
-let qrCode = generator.generateQRCode(from: input)
-try generator.saveImage(qrCode!, to: path)
-
-exit(0)
